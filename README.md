e673b89605a98710088cae65aa0ab0dbcd8dcc9b
Jianguo Ma <jiangma@microsoft.com>
2022-03-03 09:59:21 +0800

    connect to azure sql with ActiveDirectoryPassword
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# javaee-on-azure-samples
+# Java on Azure Samples
+
+This repo is for hosting Java on Azure samples developed when engaging with customers.
+
+## Connect to Azure SQL
+
+The samples below describe different approaches to connect to Azure SQL database.
+
+1. [Connect to Azure SQL with authentication `ActiveDirectoryPassword`](./sql-auth-aad-password/README.md)
---
+++ sql-auth-aad-password/src/main/java/com/example/samples/App.java
@@ -0,0 +1,28 @@
+package com.example.samples;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName(System.getenv("DB_SERVER_NAME")); // Replace with your server name
+        ds.setDatabaseName(System.getenv("DB_NAME")); // Replace with your database
+        ds.setUser(System.getenv("DB_USER")); // Replace with your user name
+        ds.setPassword(System.getenv("DB_PASSWORD")); // Replace with your password
+        ds.setAuthentication("ActiveDirectoryPassword");
+
+        try (Connection connection = ds.getConnection();
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT SUSER_SNAME()")) {
+            if (rs.next()) {
+                System.out.println("You have successfully logged on as: " + rs.getString(1));
+            }
+        }
+    }
+}
