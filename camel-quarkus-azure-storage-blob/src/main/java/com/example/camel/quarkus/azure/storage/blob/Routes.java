74ca54ab20c21e8bfbb11e5deb2e7a8fe8a49f4d
Jianguo Ma <jiangma@microsoft.com>
2024-12-03 16:47:57 +0800

    add sample camel-quarkus-azure-storage-blob
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

---
+++ camel-quarkus-azure-storage-blob/src/main/java/com/example/camel/quarkus/azure/storage/blob/Routes.java
@@ -0,0 +1,52 @@
+package com.example.camel.quarkus.azure.storage.blob;
+
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.io.IOUtils;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.jboss.logging.Logger;
+
+/**
+ * camel azure-storage-blob routes.
+ */
+@ApplicationScoped
+public class Routes extends RouteBuilder {
+
+    private static final Logger LOG = Logger.getLogger(Routes.class);
+
+    @ConfigProperty(name = "azure.storage.blob.endpoint")
+    String endpoint;
+
+    @ConfigProperty(name = "account.name")
+    String accountName;
+
+    @ConfigProperty(name = "container.name")
+    String containerName;
+
+    @ConfigProperty(name = "blob.name")
+    String blobName;
+
+    @Override
+    public void configure() {
+        // See https://learn.microsoft.com/azure/developer/java/sdk/authentication/azure-hosted-apps#defaultazurecredential
+        BlobServiceClient client = new BlobServiceClientBuilder()
+                .endpoint(endpoint)
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+        getContext().getRegistry().bind("client", client);
+
+        fromF("azure-storage-blob://%s/%s?blobName=%s&serviceClient=#client", accountName, containerName, blobName)
+                .process(exchange -> {
+                    InputStream is = exchange.getMessage().getBody(InputStream.class);
+                    LOG.infof("Downloaded blob content: %s", IOUtils.toString(is, StandardCharsets.UTF_8));
+                })
+                .end();
+    }
+}
